6a32eedbe2329679bdbe1a36ccc053d7539208be
vmvpro <melnvetal@gmail.com>
2020-04-24 11:18:10 +0300

    ДЕЛЕГИРОВАНИЕ СОБЫТИЙ

--- WebProjectVMV/source/js/my/script07.js
+++ WebProjectVMV/source/js/my/script07.js
@@ -50,3 +50,18 @@ $(function () {
 });
 
 //--------------------------------------
+// ДЕЛЕГИРОВАНИЕ СОБЫТИЙ
+$(function () {
+	var listItem, itemStatus, eventType;
+	$('ul').on(
+		'click mouseover',
+		':not(#four)',
+		{ status: 'important' },
+		function (e) {
+			listItem = 'Элемент: ' + e.target.textContent + '<br />';
+			itemStatus = 'Состояние: ' + e.data.status + '<br />';
+			eventType = 'Событие: ' + e.type;
+			$('#notes').html(listItem + itemStatus + eventType);
+		}
+	);
+});
