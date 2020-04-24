a14dff04bd095f53e78559e00fc01f5f9f0d6d04
vmvpro <melnvetal@gmail.com>
2020-04-24 14:02:57 +0300

    Закрепление материала

--- WebProjectVMV/source/js/my/script07.js
+++ WebProjectVMV/source/js/my/script07.js
@@ -78,14 +78,15 @@
 //	});
 //});
 
-// ИСПОЛЬЗОВАНИЕ МЕТОДА.ANIMATE()
-$(function () {
-	$('li').on('click', function () {
-		$(this).animate({
-			opacity: 0.0,
-			paddingLeft: '+=80'
-		}, 500, function () {
-			$(this).remove();
-		});
-	});
-});
+//// ИСПОЛЬЗОВАНИЕ МЕТОДА.ANIMATE()
+//$(function () {
+//	$('li').on('click', function () {
+//		$(this).animate({
+//			opacity: 0.0,
+//			paddingLeft: '+=80'
+//		}, 500, function () {
+//			$(this).remove();
+//		});
+//	});
+//});
+
